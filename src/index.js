87a654544e62dd70e6f6b581c926c0f7160f4cef
Fredrik <epost.larsson@gmail.com>
2020-07-05 21:23:59 +0200

    ready to use boilerplate for html, js, scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import App from "./js/components/App";
-import "./js/test.js";
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import App from "./js/App";
 import "./scss/main.scss";
+
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
